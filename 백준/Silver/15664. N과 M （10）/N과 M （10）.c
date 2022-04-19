8b5f2e729b0fcbc51a9504f0622cd527667c55f5
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 20:11:28 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int array[8];
+int set[8] = {0,};
+int N, M;
+
+int compare(const void* a, const void* b) {
+    int num1 = *(int*)a;
+    int num2 = *(int*)b;
+
+    if(num1 > num2) {
+        return 1;
+    }
+
+    if(num1 < num2) {
+        return -1;
+    }
+
+    return 0;
+}
+
+void func(int index, int turn) {
+    if(index == M) {
+        for(int i = 0; i < M; i++) {
+            printf("%d ", set[i]);
+        }
+        printf("\n");
+        return;
+    }
+
+    for(int i = turn; i < N; i++) {
+        if(array[i] != set[index]) {
+            set[index] = array[i];
+            func(index + 1, i + 1);
+        }
+    }
+
+    set[index] = 0;
+}
+
+int main() {
+    scanf("%d %d", &N, &M);
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &array[i]);
+    }
+    qsort(array, N, sizeof(int), compare);
+    func(0, 0);
+}
---
+++ 백준/Silver/15664. N과 M （10）/README.md
@@ -0,0 +1,12 @@
+# [Silver II] N과 M (10) - 15664 
+
+[문제 링크](https://www.acmicpc.net/problem/15664) 
+
+### 성능 요약
+
+메모리: 1116 KB, 시간: 0 ms
+
+### 분류
+
+백트래킹(backtracking)
+

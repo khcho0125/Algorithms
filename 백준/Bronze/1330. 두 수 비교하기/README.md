b2bdc1db8440a787eefc1ef7153f038ec3e9456f
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 19:06:26 +0900

    [Bronze IV] Title: 두 수 비교하기, Time: 208 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/README.md
@@ -0,0 +1,12 @@
+# [Bronze IV] 두 수 비교하기 - 1330 
+
+[문제 링크](https://www.acmicpc.net/problem/1330) 
+
+### 성능 요약
+
+메모리: 17696 KB, 시간: 208 ms
+
+### 분류
+
+사칙연산(arithmetic), 구현(implementation), 수학(math)
+
---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        if(a > b) {
+            System.out.println(">");
+        }
+        else if(a < b) {
+            System.out.println("<");
+        }
+        else {
+            System.out.println("==");
+        }
+        scan.close();
+    }
+}

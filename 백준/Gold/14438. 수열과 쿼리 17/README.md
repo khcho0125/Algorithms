d9db3d222b9f9ac15116cdc4c9fc37690ab27f68
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 20:06:00 +0900

    [Gold I] Title: 수열과 쿼리 17, Time: 92 ms, Memory: 3460 KB -BaekjoonHub

---
+++ 백준/Gold/14438. 수열과 쿼리 17/README.md
@@ -0,0 +1,12 @@
+# [Gold I] 수열과 쿼리 17 - 14438 
+
+[문제 링크](https://www.acmicpc.net/problem/14438) 
+
+### 성능 요약
+
+메모리: 3460 KB, 시간: 92 ms
+
+### 분류
+
+자료 구조(data_structures), 세그먼트 트리(segtree)
+
---
+++ 백준/Gold/14438. 수열과 쿼리 17/수열과 쿼리 17.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+#define ll long long
+
+int tree[400004];
+ll array[100001];
+int N;
+
+int compare(int a, int b) {
+    return array[a] < array[b] ? a : b;
+}
+
+int segment(int start, int end, int index) {
+    if(start == end) return tree[index] = end;
+
+    int mid = (start + end) / 2;
+    int one = segment(start, mid, index * 2);
+    int two = segment(mid + 1, end, index * 2 + 1);
+    tree[index] = compare(one, two);
+    return tree[index];
+} 
+
+int find(int start, int end, int s, int e, int index) {
+    if(start > e || end < s) return -1;
+    if(s <= start && end <= e) return tree[index];
+    int mid = (start + end) / 2;
+    int one = find(start, mid, s, e, index * 2);
+    int two = find(mid + 1, end, s, e, index * 2 + 1);
+    if(one == -1) return two;
+    if(two == -1) return one;
+    return compare(one, two);
+}
+
+void update(int start, int end, int i, int index) {
+    if(start > i || end < i) return;
+    if(start == end) return;  
+    int mid = (start + end) / 2;
+    update(start, mid, i, index * 2);
+    update(mid + 1, end, i, index * 2 + 1);
+    tree[index] = compare(tree[index * 2], tree[index * 2 + 1]);
+}
+
+int main() {
+    int M, num, i, j;
+    scanf("%d", &N);
+    for(int n = 1; n <= N; n++) {
+        scanf("%lld", &array[n]);
+    }
+    segment(1, N, 1);
+    scanf("%d", &M);
+    for(int m = 0; m < M; m++) {
+        scanf("%d %d", &num, &i);
+        if(num == 1) {
+            scanf("%lld", &array[i]);
+            update(1, N, i, 1);
+        }
+        else {
+            scanf("%d", &j);
+            printf("%lld\n", array[find(1, N, i, j, 1)]);
+        }
+    }
+}

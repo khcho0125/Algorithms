aa2dd6f0cf4d111546e74c4127557a4df6b19ddb
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 20:07:33 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] 와이버스 부릉부릉 - 14645 
+
+[문제 링크](https://www.acmicpc.net/problem/14645) 
+
+### 성능 요약
+
+메모리: 1112 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    for(int i = 0; i < N; i++) {
+        scanf("%d %d", &M, &M);
+    }
+    printf("비와이");
+}

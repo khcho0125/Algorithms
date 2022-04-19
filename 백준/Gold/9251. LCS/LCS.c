cecce657f04556926fa6b1167b650bdf67ff78af
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 19:39:25 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5024 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+
+int LCS[1001][1001] = {0,};
+
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+int main() {
+    char arr[2][1001];
+    int result = 0;
+    scanf("%s", &arr[0]);
+    scanf("%s", &arr[1]);
+    int len0 = strlen(arr[0]);
+    int len1 = strlen(arr[1]);
+    for(int i = 1; i <= len1; i++) {
+        for(int j = 1; j <= len0; j++) {
+            if(arr[1][i - 1] == arr[0][j - 1]) {
+                LCS[i][j] = LCS[i - 1][j - 1] + 1;
+                result = max(result, LCS[i][j]);
+            }
+            else {
+                LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1]);
+            }
+        }
+    }
+    printf("%d", result);
+}
---
+++ 백준/Gold/9251. LCS/README.md
@@ -0,0 +1,12 @@
+# [Gold V] LCS - 9251 
+
+[문제 링크](https://www.acmicpc.net/problem/9251) 
+
+### 성능 요약
+
+메모리: 5024 KB, 시간: 4 ms
+
+### 분류
+
+다이나믹 프로그래밍(dp)
+

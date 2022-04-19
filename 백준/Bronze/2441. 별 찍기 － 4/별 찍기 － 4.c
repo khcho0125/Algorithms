1b61641ae202f8d5ec9c0b5c3c1f77d937d5da24
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 19:21:16 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++)
+    {
+        for(int k=1; k <= i; k++)
+        {
+            printf(" ");
+        }
+        for(int j=1; j<=(n-i); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

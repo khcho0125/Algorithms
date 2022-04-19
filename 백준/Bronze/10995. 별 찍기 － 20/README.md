137514a5c4fd13dc3471b170907837e83708f419
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 19:51:42 +0900

    [Bronze II] Title: 별 찍기 - 20, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/README.md
@@ -0,0 +1,12 @@
+# [Bronze II] 별 찍기 - 20 - 10995 
+
+[문제 링크](https://www.acmicpc.net/problem/10995) 
+
+### 성능 요약
+
+메모리: 1116 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i < n; i++)
+    {
+        for(int j=0; j < n; j++)
+        {
+            if(i%2==1)
+            {
+                printf(" *");
+            }
+            else
+            {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}

67afefc8585b39953b7a657bb5d432ba90766361
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 20:12:06 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/README.md
@@ -0,0 +1,12 @@
+# [Bronze IV] 연세대학교 - 15680 
+
+[문제 링크](https://www.acmicpc.net/problem/15680) 
+
+### 성능 요약
+
+메모리: 1112 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    if(N) {
+        printf("Leading the Way to the Future");
+    } else {
+        printf("YONSEI");
+    }
+}

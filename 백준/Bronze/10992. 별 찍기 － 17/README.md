7d1ab95d2f428941a5ce4edf6a808ee253ee4e07
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 19:51:17 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] 별 찍기 - 17 - 10992 
+
+[문제 링크](https://www.acmicpc.net/problem/10992) 
+
+### 성능 요약
+
+메모리: 1116 KB, 시간: 0 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int sud;
+    scanf("%d", &sud);
+    for(int i=0; i < sud; i++)
+    {
+        for(int j=1; j <= sud-(i+1); j++)
+        {
+            printf(" ");
+        }
+        printf("*");
+        for(int k=1; k <= i*2-1; k++)
+        {
+            if(i==sud-1){
+                printf("*");
+            }
+            else
+                printf(" ");
+        }
+        if(i!=0)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}

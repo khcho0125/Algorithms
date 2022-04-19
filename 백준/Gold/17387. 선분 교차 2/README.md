aa09938c335b37efe367ff9ef93910a50903facd
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-19 20:24:23 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/README.md
@@ -0,0 +1,12 @@
+# [Gold II] 선분 교차 2 - 17387 
+
+[문제 링크](https://www.acmicpc.net/problem/17387) 
+
+### 성능 요약
+
+메모리: 1116 KB, 시간: 0 ms
+
+### 분류
+
+많은 조건 분기(case_work), 기하학(geometry), 선분 교차 판정(line_intersection)
+
---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+#define ll long long
+#define min(a, b) ((a < b) ? a : b)
+#define max(a, b) ((a > b) ? a : b)
+
+typedef struct pair {
+    ll x, y;
+}pair;
+
+int ccw(pair one, pair two, pair three) {
+    ll a = (one.x * two.y) + (two.x * three.y) + (three.x * one.y);
+    ll b = (one.y * two.x) + (two.y * three.x) + (three.y * one.x);
+    ll value = a - b;
+    return (value > 0) ? 1 : (value < 0 ? -1 : 0);
+}
+
+int onSegments(pair a, pair b, pair c) {
+
+    if(c.x >= min(a.x, b.x) && c.x <= max(a.x, b.x) && c.y >= min(a.y, b.y) && c.y <= max(a.y, b.y)) return 1;
+
+    return 0;
+}
+
+int Intersect(pair L1_first, pair L1_second, pair L2_first, pair L2_second) {
+    int d1, d2, d3, d4;
+    d1 = ccw(L1_first, L2_first, L2_second);
+    d2 = ccw(L1_second, L2_first, L2_second);
+    d3 = ccw(L2_first, L1_first, L1_second);
+    d4 = ccw(L2_second, L1_first, L1_second);
+    if(d1 * d2 < 0 && d3 * d4 < 0) return 1;
+    if(!d1 && onSegments(L2_first, L2_second, L1_first)) return 1;
+    if(!d2 && onSegments(L2_first, L2_second, L1_second)) return 1;
+    if(!d3 && onSegments(L1_first, L1_second, L2_first)) return 1;
+    if(!d4 && onSegments(L1_first, L1_second, L2_second)) return 1;
+
+    return 0;
+}
+
+int main() {
+    pair P[4];
+    for(int i = 0; i < 4; i++) {
+        scanf("%lld %lld", &P[i].x, &P[i].y);
+    }
+    printf("%d", Intersect(P[0], P[1], P[2], P[3]));
+}

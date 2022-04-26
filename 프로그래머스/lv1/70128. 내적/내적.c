56854eee6cf95b99cc8b09580adfbc2b50b20eda
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-26 16:18:55 +0900

    [level 1] Title: 내적, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/70128. 내적/내적.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// a_len은 배열 a의 길이입니다.
+// b_len은 배열 b의 길이입니다.
+int solution(int a[], size_t a_len, int b[], size_t b_len) {
+    int answer = 0;
+    for(int i = 0; i < a_len; i++) {
+        answer += a[i] * b[i];
+    }
+    return answer;
+}

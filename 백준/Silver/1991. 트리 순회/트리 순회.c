4f9471b286b030e2808accdbd426e0fb48849ba5
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2022-06-02 23:08:38 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.c
+++ 백준/Silver/1991. 트리 순회/트리 순회.c
@@ -1,68 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct baekjoon {
-    char value;
-    int Llink;
-    int Rlink;
-}node;
+char TREE[26][2] = {0,};
 
-typedef struct Head {
-    node* link;
-}head;
-
-head tree[26];
-
-void first(int N) {
-    printf("%c", tree[N].link->value);
-    if(tree[N].link->Llink != -1) {
-        first(tree[N].link->Llink);
+void first(int idx) {
+    printf("%c", idx + 'A');
+    if(TREE[idx][0] != '.') {
+        first(TREE[idx][0] - 'A');
     }
-    if(tree[N].link->Rlink != -1) {
-        first(tree[N].link->Rlink);
+    if(TREE[idx][1] != '.') {
+        first(TREE[idx][1] - 'A');
     }
 }
 
-void second(int N) {
-    if(tree[N].link->Llink != -1) {
-        second(tree[N].link->Llink);
+void middle(int idx) {
+    if(TREE[idx][0] != '.') {
+        middle(TREE[idx][0] - 'A');
     }
-    printf("%c", tree[N].link->value);
-    if(tree[N].link->Rlink != -1) {
-        second(tree[N].link->Rlink);
-    }   
+    printf("%c", idx + 'A');
+    if(TREE[idx][1] != '.') {
+        middle(TREE[idx][1] - 'A');
+    }
 }
 
-void third(int N) {
-    if(tree[N].link->Llink != -1) {
-        third(tree[N].link->Llink);
+void last(int idx) {
+    if(TREE[idx][0] != '.') {
+        last(TREE[idx][0] - 'A');
     }
-    if(tree[N].link->Rlink != -1) {
-        third(tree[N].link->Rlink);
+    if(TREE[idx][1] != '.') {
+        last(TREE[idx][1] - 'A');
     }
-    printf("%c", tree[N].link->value);   
+    printf("%c", idx + 'A');
 }
 
 int main() {
     int N;
-    char parent, L, R;
+    char one;
     scanf("%d", &N);
-    for(int i = 0; i < 26; i++) {
-        tree[i].link = NULL;
-    }
     for(int i = 0; i < N; i++) {
-        scanf(" %c %c %c", &parent, &L, &R);
-        node* newNode = (node*)malloc(sizeof(node));
-        newNode->value = parent;
-        if(L != '.') newNode->Llink = L - 'A';
-        else newNode->Llink = -1;
-        if(R != '.') newNode->Rlink = R - 'A';
-        else newNode->Rlink = -1;
-        tree[parent - 'A'].link = newNode;
+        scanf(" %c", &one);
+        scanf(" %c %c", &TREE[one - 'A'][0], &TREE[one - 'A'][1]);
     }
     first(0);
     printf("\n");
-    second(0);
+    middle(0);
     printf("\n");
-    third(0);
+    last(0);
 }

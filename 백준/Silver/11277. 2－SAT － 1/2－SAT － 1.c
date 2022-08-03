b9d8cbd005422c20eff6f027f873da9f9a9cafcd
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2022-08-03 13:47:24 +0900

    [Silver I] Title: 2-SAT - 1, Time: 16 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/11277. 2－SAT － 1/2－SAT － 1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+typedef struct Pair {
+    int first, second;
+}pair;
+
+pair clause[100];
+
+int compare(int bit, int idx) {
+
+    int one = clause[idx].first < 0 ? !(bit & 1 << (clause[idx].first * -1 - 1)) : bit & 1 << (clause[idx].first - 1);
+    int two = clause[idx].second < 0 ? !(bit & 1 << (clause[idx].second * -1 - 1)) : bit & 1 << (clause[idx].second - 1);
+
+    return one | two;
+}
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    for(int i = 0; i < M; i++) {
+        scanf("%d %d", &clause[i].first, &clause[i].second);
+    }
+
+    for(int i = 0, idx = 0; i < 1 << 20; i++, idx = 0) {
+        for(; compare(i, idx) && idx != M; idx++);
+
+        if(idx == M) {
+            printf("1");
+            return 0;
+        }
+    }
+    
+    printf("0");
+}

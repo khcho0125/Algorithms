7b7941c3d03c9c003a4b7bc25ef5dfbcec577cf1
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2022-09-03 12:56:14 +0900

    [Platinum II] Title: 나무 자르기, Time: 48 ms, Memory: 6496 KB -BaekjoonHub

--- 백준/Platinum/13263. 나무 자르기/나무 자르기.c
+++ 백준/Platinum/13263. 나무 자르기/나무 자르기.c
@@ -5,48 +5,56 @@
 #define MAX 100000
 
 typedef struct Line {
-    ll a;
-    ll b;
+    ll a, b;
     double s;
 }Line;
 
 ll dp[MAX] = {0,};
-ll a[MAX];
-ll b[MAX];
+int a[MAX];
+int b[MAX];
 Line* F[MAX];
 int temp = 0;
 
 double cross(Line* a, Line* b) {
-    return (-1) * ((a->b - b->b) / (a->a - b->a));
+    return (b->b - a->b) / (a->a - b->a);
+}
+
+int lowerBound(int idx) {
+    int low = 0, high = temp - 1;
+    while(low + 1 < high) {
+        int mid = (low + high) / 2;
+        if(a[idx] < F[mid]->s) high = mid;
+        else low = mid;
+    }
+    return low;
 }
 
 int main() {
     int N;
     scanf("%d", &N);
     for(int i = 0; i < N; i++) {
-        scanf("%lld", &a[i]);
+        scanf("%d", &a[i]);
     }
     for(int i = 0; i < N; i++) {
-        scanf("%lld", &b[i]);
+        scanf("%d", &b[i]);
     }
 
-    int fpos = 0;
     for(int i = 1; i < N; i++) {
         Line* s = (Line*)malloc(sizeof(Line));
         s->a = b[i - 1];
         s->b = dp[i - 1];
+        s->s = 0;
         while(temp > 0) {
             s->s = cross(F[temp - 1], s);
             
             if(F[temp - 1]->s < s->s) break;
             
-            if(--temp == fpos) fpos--; 
+            --temp;
         }
         F[temp++] = s;
 
-        while(fpos + 1 < temp && F[fpos + 1]->s < a[i]) {
-            fpos++;
-        }
+        int fpos = temp - 1;
+        if(a[i] < F[fpos]->s) fpos = lowerBound(i);
 
         dp[i] = F[fpos]->a * a[i] + F[fpos]->b;
     }

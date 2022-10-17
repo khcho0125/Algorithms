3868164941182b991d181297800eb2cfbcf062fd
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2022-10-17 12:20:08 +0900

    [level 1] Title: 직사각형 별찍기, Time: 2.40 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    for(int i = 0; i < b; i++) {
+        for(int j = 0; j < a; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

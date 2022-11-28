cddf58e74a742593bf0b85b997ac04c99a9c20c2
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2022-11-28 19:33:34 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 36 ms, Memory: 7236 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.c
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.c
@@ -1,40 +1,36 @@
 #include<stdio.h>
-#include<string.h>
 #include<stdbool.h>
 
-#define INF 987654321
+#define min(a, b) (a < b ? a : b)
 
-bool dp[2501][2501] = {0,};
+int D[2501] = {0,};
+bool P[2501][2501];
+char str[2501];
 
 int main() {
-    char str[2501];
-    int count[2501];
+    int len = 0;
     scanf("%s", str);
-    int len = strlen(str);
-    for(int i = 0; i < len; i++) {
-        dp[i][i] = true;
+
+    for(int i = 0; str[i] != '\0'; i++, len++) P[i][i] = true;
+    
+    for(int i = 0; i < len - 1; i++) {
+        P[i][i + 1] = str[i] == str[i + 1];
     }
-    for(int i = 1; i < len; i++) {
-        if(str[i] == str[i - 1]) {
-            dp[i - 1][i] = true;
-        }
-    }
+
     for(int i = 2; i < len; i++) {
-        for(int j = 0; j < len; j++) {
-            int k = i + j;
-            if(str[j] == str[k] && dp[j + 1][k - 1]) {
-                dp[j][k] = true;
-            }
+        for(int j = 0; j < len - i; j++) {
+            P[j][j + i] = str[j] == str[j + i] && P[j + 1][j + i - 1];
         }
     }
-    count[0] = 0;
-    for(int i = 1; i <= len; i++) {
-        count[i] = INF;
-        for(int j = 0; j < i; j++) {
-            if(dp[j][i - 1] && count[i] > count[j] + 1) {
-                count[i] = count[j] + 1;
-            }
+
+    D[0] = 1;
+    for(int i = 1; i < len; i++) {
+        D[i] = D[i - 1] + 1;
+        for(int j = 1; j <= i; j++) {
+            if(P[i - j][i]) D[i] = min(D[i], D[i - j - 1] + 1);
         }
     }
-    printf("%d", count[len]);
+
+    printf("%d", D[len - 1]);
+
 }

f665728d876e5f34c8804f0076a96945709e9205
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2022-12-23 21:34:56 +0900

    [Silver V] Title: 평행사변형, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

--- 백준/Silver/1064. 평행사변형/평행사변형.c
+++ 백준/Silver/1064. 평행사변형/평행사변형.c
@@ -1,25 +1,47 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
-int main()
-{
-    int xA, yA, xB, yB, xC, yC;
-    double g, h, k;
-    scanf("%d %d %d %d %d %d", &xA, &yA, &xB, &yB, &xC, &yC);
+#define max(a, b) (a > b ? a : b)
+#define min(a, b) (a < b ? a : b)
 
-    if(fabs(yB - yA) / fabs(xB - xA) == fabs(yC - yB) / fabs(xC - xB) || (xA == xB && xB == xC) || (yA == yB && yB == yC)) {
+typedef struct Point {
+    int x, y;
+}Point;
+
+typedef struct Line {
+    double w, l;
+}Line;
+
+Line line(Point A, Point B) {
+    double x = fabs(A.x - B.x);
+    double y = fabs(A.y - B.y);
+
+    return (Line){
+        .w = y / x,
+        .l = sqrt(x * x + y * y)
+    };
+}
+
+int main() {
+    Point A, B, C;
+
+    scanf("%d %d %d %d %d %d", &A.x, &A.y, &B.x, &B.y, &C.x, &C.y);
+    
+    Line AB = line(A, B);
+    Line BC = line(B, C);
+    Line AC = line(A, C);
+
+    if (AB.w == BC.w && AB.w == AC.w) {
         printf("-1");
         return 0;
     }
 
-    g = sqrt(pow((xA > xB ? xA - xB : xB - xA), 2) + pow((yA > yB ? yA - yB : yB - yA), 2));
-    h = sqrt(pow((xA > xC ? xA - xC : xC - xA), 2) + pow((yA > yC ? yA - yC : yC - yA), 2));
-    k = sqrt(pow((xC > xB ? xC - xB : xB - xC), 2) + pow((yC > yB ? yC - yB : yB - yC), 2));
-    if(g > h && g > k) {
-        printf("%.9lf", (h > k ? g - k : g - h) * 2);
-    } else if(h > g && h > k) {
-        printf("%.9lf", (g > k ? h - k : h - g) * 2);
-    } else {
-        printf("%.9lf", (h > g ? k - g : k - h) * 2);
-    }
+    double one = (AB.l + BC.l);
+    double two = (AB.l + AC.l);
+    double three = (BC.l + AC.l);
+
+    double result = (max(one, max(two, three)) - min(one, min(two, three))) * 2;
+
+    printf("%.9lf", result);
+
 }

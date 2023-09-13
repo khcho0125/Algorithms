19fbea6244b331fe5b911b10a427c323514b2c0a
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2023-09-14 00:31:29 +0900

    [Platinum III] Title: 책 구매하기 3, Time: 384 ms, Memory: 1636 KB -BaekjoonHub

---
+++ 백준/Platinum/11407. 책 구매하기 3/책 구매하기 3.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+#define N_MAX 101
+#define INF 987654321
+#define min(a, b) ((a) < (b) ? a : b)
+
+int capacity[N_MAX * 2][N_MAX * 2];
+int flow[N_MAX * 2][N_MAX * 2];
+int charge[N_MAX * 2][N_MAX * 2];
+
+int queue[N_MAX * N_MAX];
+int visited[N_MAX * 2];
+int dist[N_MAX * 2];
+bool inQueue[N_MAX * 2];
+
+void spfa(int source, int sink) {
+    memset(visited, -1, sizeof(visited));
+
+    for(int i = 0; i <= sink; i++) dist[i] = INF;
+    dist[source] = 0;
+
+    int front = 0, rear = 0, u;
+    queue[rear++] = source;
+    while(front < rear) {
+        u = queue[front++];
+        inQueue[u] = false;
+
+        for(int v = 1; v <= sink; v++) {
+            if(capacity[u][v] - flow[u][v] > 0 && dist[v] > dist[u] + charge[u][v]) {
+                dist[v] = dist[u] + charge[u][v];
+                visited[v] = u;
+
+                if(!inQueue[v]) {
+                    queue[rear++] = v;
+                    inQueue[v] = true;
+                }
+            }
+        }
+    }
+}
+
+void mcmf(int source, int sink) {
+    int cost = 0, result = 0;
+
+    int book;
+    while(true) {
+        spfa(source, sink);
+
+        if(visited[sink] == -1) break;
+
+        for(int i = sink; i != source; i = visited[i]) {
+            book = min(book, capacity[visited[i]][i] - flow[visited[i]][i]);
+        }
+
+        for(int i = sink; i != source; i = visited[i]) {
+            cost += book * charge[visited[i]][i];
+            flow[visited[i]][i] += book;
+            flow[i][visited[i]] -= book;
+        }
+
+        result += book;
+    }
+
+    printf("%d\n%d", result, cost);
+}
+
+int main() {
+    int N, M, A, B, C, D;
+    scanf("%d %d", &N, &M);
+
+    int source = 0, sink = N + M + 1;
+    for(int i = 1; i <= N; i++) {
+        scanf("%d", &A);
+        capacity[M + i][sink] = A;
+    }
+    for(int i = 1; i <= M; i++) {
+        scanf("%d", &B);
+        capacity[source][i] = B;
+    }
+    for(int i = 1; i <= M; i++) {
+        for(int j = 1; j <= N; j++) {
+            scanf("%d", &C);
+            capacity[i][M + j] = C;
+        }
+    }
+    for(int i = 1; i <= M; i++) {
+        for(int j = 1; j <= N; j++) {
+            scanf("%d", &D);
+            charge[i][M + j] = D;
+            charge[M + j][i] = -D;
+        }
+    }
+    
+    mcmf(source, sink);
+}

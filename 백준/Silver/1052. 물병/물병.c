9dbcd4c6d6ad7350e939f964c60ca8d98d1fe824
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2023-11-15 11:41:56 +0900

    [Silver I] Title: 물병, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.c
+++ 백준/Silver/1052. 물병/물병.c
@@ -1,34 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include<stdio.h>
 
-int main()
-{
-    int bottle = 0;
-    int N, K, i, len, temp;
-    int cnt = 0;
+int calculateBottle(int n) {
+    return n ? calculateBottle(n - (-n & n)) + 1 : 0;
+}
+
+int main() {
+    int N, K, temp;
     scanf("%d %d", &N, &K);
-    for (cnt = 0, temp = N; temp != 0;)
-    {
-        if (temp % 2 != 0)
-        {
-            cnt++;
-        }
-        temp /= 2;
-    }
-    while (cnt > K)
-    {
-        bottle += N & (-N);
-        N += N & (-N);
-        for (cnt = 0, temp = N; temp != 0; )
-        {
-            if (temp % 2 != 0)
-            {
-                cnt++;
-            }
-            temp /= 2;
-        }
-    }
-    printf("%d", bottle);
-    return 0;
+    for(temp = N; calculateBottle(temp) > K; temp += -temp & temp);
+    printf("%d", temp - N);
 }

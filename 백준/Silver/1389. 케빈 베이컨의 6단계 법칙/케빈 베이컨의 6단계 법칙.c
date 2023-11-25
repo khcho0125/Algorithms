6efff71149d21d7b01e4f39f71a2c9a24c184d47
KyungHyeon <82090641+khcho0125@users.noreply.github.com>
2023-11-25 21:15:14 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 0 ms, Memory: 1156 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.c
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.c
@@ -1,68 +1,53 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int N;
-int arr[100][100] = {
-    0,
-};
+#define INF 987654321
+#define N_MAX 101
 
-int bacon[100][100] = {
-    0,
-};
+#define min(a, b) ((a) < (b) ? a : b)
 
-int queue[5000] = {
-    0,
-};
-int queuefront = 0;
-int queueback = 0;
+int dist[N_MAX][N_MAX];
 
-void BFS(int data, int V, int idx)
-{
-    int have = 0, i;
-    queue[queuefront] = 0;
-    queuefront++;
-    for (i = 0; i < N; i++)
-    {
-        if (data != i && arr[V][i] && (bacon[data][i] > idx || !bacon[data][i]))
-        {
-            queue[queueback] = i;
-            queueback++;
-            bacon[data][i] = idx;
-            BFS(data, queue[queuefront], idx + 1);
+void floyd(int N) {
+    for (int k = 1; k <= N; k++) {
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= N; j++) {
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
         }
     }
 }
 
+int solution(int N) {
+    floyd(N);
+
+    int result, temp = INF;
+    for (int i = 1, sum = 0; i <= N; i++, sum = 0) {
+        for (int j = 1; j <= N; j++) {
+            sum += dist[i][j];
+        }
+        
+        if (sum < temp) {
+            temp = sum;
+            result = i;
+        }
+    }
+    return result;
+}
+
 int main()
 {
-    int M, A, B, result, temp, sum, max, i, j, idx;
+    int N, M, A, B;
     scanf("%d %d", &N, &M);
-    for (i = 0; i < M; i++)
-    {
+    for(int i = 1; i <= N; i++) {
+        for(int j = 1; j <= N; j++) {
+            dist[i][j] = (i == j ? 0 : INF);
+        }
+    }
+
+    for(int i = 0; i < M; i++) {
         scanf("%d %d", &A, &B);
-        arr[A - 1][B - 1] = 1;
-        arr[B - 1][A - 1] = 1;
+        dist[A][B] = dist[B][A] = 1;
     }
-    for (i = 0; i < N; i++)
-    {
-        queue[queueback] = i;
-        queueback++;
-        BFS(i, i, 1);
-        for (j = 0; j < N; j++)
-        {
-            queue[j] = 0;
-        }
-    }
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0, sum = 0; j < N; j++)
-        {
-            sum += bacon[i][j];
-        }
-        if (sum < max || !i)
-        {
-            max = sum;
-            result = i + 1;
-        }
-    }
-    printf("%d", result);
+
+    printf("%d", solution(N));
 }
